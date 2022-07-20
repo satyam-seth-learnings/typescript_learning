be9291ebaa411d4e76a2626bd2065dbd3c7f0dce
satyam-seth <satyam1998.1998@gmail.com>
2022-07-20 23:26:16 +0530

    functions as type

--- Smartherd/Chapter-3/app/main.ts
+++ Smartherd/Chapter-3/app/main.ts
@@ -2,6 +2,7 @@
 function startGame() {
   let playerName: string | undefined = getInputVal("playerName");
   displayScore(10, playerName);
+  displayScore(-10, playerName);
 }
 
 document.getElementById("startGame")!.addEventListener("click", startGame);
@@ -30,7 +31,20 @@ function getInputVal(elementID: string): string | undefined {
  * @return void
  */
 function displayScore(score: number, playerName: string = "Math Player"): void {
+  let logger: (value: string) => void;
+
+  if (score < 0) {
+    logger = logError;
+  } else {
+    logger = logMessage;
+  }
+
+  logger(`Score -> ${score}`);
+
   const scoreElement: HTMLElement | null =
     document.getElementById("playerScore");
   scoreElement!.innerText = `${playerName}, your score -> ${score}`;
 }
+
+let logMessage = (message: string) => console.log(message);
+let logError = (message: string) => console.error(message);
