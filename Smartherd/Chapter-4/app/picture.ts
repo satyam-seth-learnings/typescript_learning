9d0ea36d8a4b53cc28ee669059bcacbc75214b2a
satyam-seth <satyam1998.1998@gmail.com>
2022-07-21 00:57:51 +0530

    extends class

--- Smartherd/Chapter-4/app/picture.ts
+++ Smartherd/Chapter-4/app/picture.ts
@@ -2,36 +2,24 @@
  * Defining class
  */
 class Picture {
-  public artTitle: string; // member are public by default
-  protected owner: string; // accessed inside the class or any classes that inherit from the class
-  private _price: number; // soft private, can't be accesses outside the class
-  #newPrice: number; // hard private property
-
-  /**
-   * Getter method: with 'get' keyword with name of the property takes no parameter
-   * returns value of the property
-   */
-  get price() {
-    return this._price;
-  }
-
-  /**
-   * Setter method: with 'set' keyword with name of the property takes ones parameter
-   * sets the value of the property
-   */
-  set price(newPrice: number) {
-    this._price = newPrice;
-  }
+  // member are public by default
+  artTitle: string;
+  owner: string;
 
   orderPicture(quantity: number): void {
     console.log(`Order ${quantity} copies of ${this.artTitle}`);
   }
 }
 
-let newPicture: Picture = new Picture();
-newPicture._price = 1000;
-newPicture["_price"] = 1000;
+/**
+ * Child class
+ */
+class Photograph extends Picture {
+  camera: string;
 
-newPicture.newPrice = 1000;
-newPicture.#newPrice = 1000;
-newPicture["#newPrice"] = 1000;
+  changeSettings(): void {
+    console.log("update camera setting");
+  }
+}
+
+let newPic: Photograph = new Photograph();
