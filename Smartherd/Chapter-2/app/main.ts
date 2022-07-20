20df13dcd18a827d46aaf62c91d6a2577a553886
satyam-seth <satyam1998.1998@gmail.com>
2022-07-20 19:48:00 +0530

    any datatype and type assertion

--- Smartherd/Chapter-2/app/main.ts
+++ Smartherd/Chapter-2/app/main.ts
@@ -13,3 +13,8 @@ document.getElementById("startGame")!.addEventListener("click", startGame);
 function welcomePlayer(name: string) {
   return `Hello ${name}! Let's start a new game...`;
 }
+
+let anyValue: any = 10;
+
+let finalString = (<number>anyValue).toFixed(5);
+let finalString2 = (anyValue as number).toFixed(5);
