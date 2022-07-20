54fa828d6cc6b1d2899281dc2a2c14b7b09dce44
satyam-seth <satyam1998.1998@gmail.com>
2022-07-20 23:15:33 +0530

    Default & Optional Parameters

--- Smartherd/Chapter-3/app/main.ts
+++ Smartherd/Chapter-3/app/main.ts
@@ -1,15 +1,36 @@
 // Function executes when Start Game button is clicked
 function startGame() {
-  let playerName: string = "Annanya";
-
-  let welcomeMessage = welcomePlayer(playerName);
-
-  var messageElement = document.getElementById("message");
-  messageElement!.innerText = welcomeMessage;
+  let playerName: string | undefined = getInputVal("playerName");
+  displayScore(10, playerName);
 }
 
 document.getElementById("startGame")!.addEventListener("click", startGame);
 
-function welcomePlayer(name: string): string {
-  return `Hello ${name}! Let's start a new game...`;
+/**
+ * Fetch input data
+ * @param elementID
+ * @return input element
+ */
+function getInputVal(elementID: string): string | undefined {
+  const inputElement: HTMLInputElement = <HTMLInputElement>(
+    document.getElementById(elementID)
+  );
+
+  if (inputElement.value === "") {
+    return undefined;
+  } else {
+    return inputElement.value;
+  }
+}
+
+/**
+ * Display score of the player
+ * @param score
+ * @param playerName
+ * @return void
+ */
+function displayScore(score: number, playerName: string = "Math Player"): void {
+  const scoreElement: HTMLElement | null =
+    document.getElementById("playerScore");
+  scoreElement!.innerText = `${playerName}, your score -> ${score}`;
 }
