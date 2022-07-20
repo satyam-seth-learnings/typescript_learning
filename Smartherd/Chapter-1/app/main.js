89f700f0e4107ea0249f0f07559569ff5edc9224
satyam-seth <satyam1998.1998@gmail.com>
2022-07-20 18:45:25 +0530

    configuration inheritance

--- Smartherd/Chapter-1/app/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// Function executes when Start Game button is clicked
-function startGame() {
-    var messageElement = document.getElementById("message");
-    messageElement.innerText = "Welcome! Starting a new game..";
-}
-document.getElementById("startGame").addEventListener("click", startGame);
-//# sourceMappingURL=main.js.map
