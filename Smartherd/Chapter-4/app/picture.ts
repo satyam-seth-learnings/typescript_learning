8bf97ad3305586e484bae9ea4749d676ad1dab3b
satyam-seth <satyam1998.1998@gmail.com>
2022-07-21 01:01:10 +0530

    implements interface

--- Smartherd/Chapter-4/app/picture.ts
+++ Smartherd/Chapter-4/app/picture.ts
@@ -12,10 +12,20 @@ class Picture {
 }
 
 /**
+ * Interface to implement
+ */
+interface Artist {
+  fullName: string;
+  age: number;
+}
+
+/**
  * Child class
  */
-class Photograph extends Picture {
+class Photograph extends Picture implements Artist {
   camera: string;
+  fullName: string;
+  age: number;
 
   changeSettings(): void {
     console.log("update camera setting");
