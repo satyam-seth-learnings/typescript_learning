716c68ffb013eb8a03d8fc0d26aa2d74a4fb66cf
satyam-seth <satyam1998.1998@gmail.com>
2022-07-20 18:22:12 +0530

    install dependencies and run your first Typescript code

---
+++ Smartherd/Chapter-1/app/main.js
@@ -0,0 +1,6 @@
+// Function executes when Start Game button is clicked
+function startGame() {
+    var messageElement = document.getElementById("message");
+    messageElement.innerText = "Welcome! Starting a new game..";
+}
+document.getElementById("startGame").addEventListener("click", startGame);
