fdb602a0d1782b8cf9d6f855c097db0b3ad16c00
satyam-seth <satyam1998.1998@gmail.com>
2022-07-20 18:36:25 +0530

    configuration options in tsconfig.js

--- Smartherd/Chapter-1/app/main.ts
+++ Smartherd/Chapter-1/app/main.ts
@@ -1,7 +1,7 @@
 // Function executes when Start Game button is clicked
 function startGame() {
   var messageElement = document.getElementById("message");
-  messageElement.innerText = "Welcome! Starting a new game..";
+  messageElement!.innerText = "Welcome! Starting a new game..";
 }
 
 document.getElementById("startGame")!.addEventListener("click", startGame);
