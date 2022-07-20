c9083117233ba1f6375c976b9d4ce11ccb13eb14
satyam-seth <satyam1998.1998@gmail.com>
2022-07-21 01:07:16 +0530

    constructor

--- Smartherd/Chapter-4/app/picture.ts
+++ Smartherd/Chapter-4/app/picture.ts
@@ -6,6 +6,10 @@ class Picture {
   static artTitle: string;
   owner: string;
 
+  constructor() {
+    console.log("Creating a new Picture");
+  }
+
   orderPicture(quantity: number): void {
     console.log(`Order ${quantity} copies of ${Picture.artTitle}`);
   }
@@ -27,10 +31,15 @@ class Photograph extends Picture implements Artist {
   fullName: string;
   age: number;
 
+  constructor(newCamera: string) {
+    super();
+    this.camera = newCamera;
+  }
+
   changeSettings(): void {
     console.log("update camera setting");
   }
 }
 
-let newPic: Photograph = new Photograph();
-Picture.artTitle;
+let newPic: Photograph = new Photograph("Sony");
+newPic.camera = "Canon g7x";
