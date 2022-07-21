cb85fb63c182830ae70e1ff8d4e2493b759bdc96
satyam-seth <satyam1998.1998@gmail.com>
2022-07-21 10:00:38 +0530

    importing and exporting modules

---
+++ Smartherd/Chapter-5/app/scoreBoard.ts
@@ -0,0 +1,19 @@
+import { Result } from "./result";
+
+export class Scoreboard {
+  private result: Result;
+
+  // results of the player
+  createResult(newResult: Result): void {
+    this.result = newResult;
+  }
+
+  // to display the score on the scoreboard
+  updateScoreboard(): void {
+    let output: string = `${this.result.playerName}: ${this.result.score}/${this.result.numberOfProblems}`;
+
+    // Fetch the element where the score needs to be displayed and update the HTML
+    const scoresElement: HTMLElement = document.getElementById("playerScore")!;
+    scoresElement.innerHTML = output;
+  }
+}
